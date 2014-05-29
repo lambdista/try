d5259b9e7f740ce697d781c3c6fae8c53a1bcde6
Alessandro Lacava <alessandrolacava@gmail.com>
2014-05-29 18:36:19 +0200

    First version completed

--- src/main/java/com/alessandrolacava/util/Success.java
+++ src/main/java/com/alessandrolacava/util/Success.java
@@ -1,5 +1,6 @@
 package com.alessandrolacava.util;
 
+import java.util.NoSuchElementException;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -32,36 +33,48 @@ public final class Success<T> extends Try<T> {
 
     @Override
     public void forEach(Consumer<? super T> action) {
-
+        action.accept(value);
     }
 
     @Override
     public <U> Try<? extends U> map(Function<? super T, ? extends U> mapper) {
-        return null;
+        return Try.apply(() -> mapper.apply(value));
     }
 
     @Override
     public <U> Try<? extends U> flatMap(Function<? super T, ? extends Try<? extends U>> mapper) {
-        return null;
+        try {
+            return mapper.apply(value);
+        } catch (Exception e) {
+            return new Failure<>(e);
+        }
     }
 
     @Override
     public Try<T> filter(Predicate<? super T> predicate) {
-        return null;
+        try {
+            if(predicate.test(value)) {
+                return this;
+            } else {
+                return new Failure(new NoSuchElementException("Predicate does not hold for " + value));
+            }
+        } catch (Exception e) {
+            return new Failure<>(e);
+        }
     }
 
     @Override
     public <U> Try<? extends U> recover(Function<Exception, ? extends U> recoverFunction) {
-        return null;
+        return (Try<U>) this;
     }
 
     @Override
     public <U> Try<? extends U> recoverWith(Function<Exception, Try<? extends U>> recoverFunction) {
-        return null;
+        return (Try<U>) this;
     }
 
     @Override
     public Try<Exception> failed() {
-        return null;
+        return new Failure<>(new UnsupportedOperationException("Success.failed"));
     }
 }
--- src/main/java/com/alessandrolacava/util/Test.java
+++ src/main/java/com/alessandrolacava/util/Test.java
@@ -1,19 +1,18 @@
 package com.alessandrolacava.util;
 
 import java.io.IOException;
-import java.util.stream.Stream;
 
 /**
  * Created by alacava on 5/29/14.
  */
 public class Test {
 
-    public static void main(String[] args) throws Throwable {
+    public static void main(String[] args) throws Exception {
 
         Try<Integer> result = Try.apply(
                 () -> test()
         );
-        System.out.println(result.getOrElse(24));
+        System.out.println(result.map((Integer a) -> a.toString() + " hello world!").get());
     }
 
     public static Integer test() throws IOException {
